c73cf20e04c3b712032a508f173977aef309e037
Saurabh Hirani <saurabh.hirani@gmail.com>
2020-10-24 13:58:23 +0530

    work in progress

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
   version = "~> 2.0"
-  region  = "eu-central-1"
+  region  = "us-east-1"
 }
 
 terraform {
   backend "s3" {
     bucket = "example-terraform-project-name-bootstrap-terraform-state"
     key    = "default-infrastructure"
-    region = "eu-central-1"
+    region = "us-east-1"
   }
 }
 
