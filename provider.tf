ef57a5cca504ee1fdc1ced0b36aba7d014f63095
Fabrice Semti <95348175+fsemti@users.noreply.github.com>
2022-06-10 16:00:33 +0100

    upgrade provider version

--- provider.tf
+++ provider.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 4.6"
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region 	= var.aws_region
 }
